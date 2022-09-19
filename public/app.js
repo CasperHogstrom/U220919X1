1869aacb95a917a3d18102cc33599a9a106c4b28
CasperHogstrom <casperhogstrom3@gmail.com>
2022-09-19 12:35:28 +0200

    First scenario working, simple for loop counting up to the chosen number

--- public/app.js
+++ public/app.js
@@ -1,1 +1,24 @@
 console.log('Hello world!')
+
+function firstBtn() {
+    var numberBoxValue = document.getElementById('numberbox').value;
+    const primeNrArray = new Array(2, 3, 5, 7 ,11);
+
+    //Eftersom arrayen börjar på 0
+    //numberBoxValue -= 1;
+
+    for (var i = 0; i < numberBoxValue; i++) {
+        console.log(primeNrArray[i]);
+    }
+    
+}
+
+function secondBtn() {
+    var numberBoxValue = document.getElementById('numberbox').value;
+    const primeNrArray = new Array(2, 3, 5, 7 ,11);
+    
+    //Eftersom arrayen börjar på 0
+    numberBoxValue -= 1;
+
+    console.log(primeNrArray[numberBoxValue]);
+}
